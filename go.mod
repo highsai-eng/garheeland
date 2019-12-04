38729e24bcb2e88752ac76313d043d1a3477c771
kaito.higa <higa@switch-m.com>
2019-12-04 17:21:39 +0900

    add walk

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/highsai-eng/garheeland
 
-require github.com/Equanox/gotron v0.2.23
+go 1.13
+
+require (
+	github.com/akavel/rsrc v0.8.0 // indirect
+	github.com/lxn/walk v0.0.0-20191128110447-55ccb3a9f5c1
+	github.com/lxn/win v0.0.0-20191128105842-2da648fda5b4 // indirect
+	golang.org/x/sys v0.0.0-20191204072324-ce4227a45e2e // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"github.com/Equanox/gotron"
+	"log"
+
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
 
 func main() {
 
-	println("test")
+	defer log.Println("destroy garheeland...")
 
-	// Create a new browser window instance
-	window, err := gotron.New()
-	if err != nil {
-		panic(err)
+	log.Println("starting garheeland...")
+
+	var inTE, outTE *walk.TextEdit
+
+	app := MainWindow{
+		Title:   "Garhee Land",
+		MinSize: Size{Width: 300, Height: 200},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &inTE},
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+		},
 	}
 
-	// Alter default window size and window title.
-	window.WindowOptions.Width = 1200
-	window.WindowOptions.Height = 980
-	window.WindowOptions.Title = "Gotron"
-
-	// Start the browser window.
-	// This will establish a golang <=> nodejs bridge using websockets,
-	// to control ElectronBrowserWindow with our window object.
-	done, err := window.Start()
-	if err != nil {
-		panic(err)
+	if _, err := app.Run(); err != nil {
+		panic(err.Error())
 	}
-
-	// Open dev tools must be used after window.Start
-	// window.OpenDevTools()
-
-	// Wait for the application to close
-	<-done
 }
